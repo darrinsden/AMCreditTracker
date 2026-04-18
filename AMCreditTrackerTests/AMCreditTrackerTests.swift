c3450aa785bf2c2fa130841b5db20556f3b7288d
darrin horn <darrinsden@gmail.com>
2026-04-18 08:24:28 -0700

    Initial Commit

---
+++ AMCreditTrackerTests/AMCreditTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  AMCreditTrackerTests.swift
+//  AMCreditTrackerTests
+//
+//  Created by Darrin horn on 4/18/26.
+//
+
+import Testing
+@testable import AMCreditTracker
+
+struct AMCreditTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
